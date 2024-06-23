d97dfc660484ec38694cf81c3391a7c82ec0095a
x23233982 <x23233982@student.ncirl.ie>
2024-06-23 19:26:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='production_optimization',
-    version='0.1.0',
+    version='0.1',
     packages=find_packages(),
     install_requires=[
-        'pulp==2.6.0',
-        'pytest==8.2.2',
-        'flake8==6.0.0'
+        'pulp',
     ],
     entry_points={
         'console_scripts': [
-            'run_optimization=production_optimization.main:run_optimization',
+            'run_optimization=production_optimization.main:main',
         ],
     },
-    classifiers=[
-        'Programming Language :: Python :: 3.10',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.10',
 )
