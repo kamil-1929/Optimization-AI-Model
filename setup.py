4aac97aae95b077564463e1df2b6939b8bd4c877
x23233982 <x23233982@student.ncirl.ie>
2024-06-23 19:06:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,19 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'pulp',
+        'pulp==2.6.0',
+        'pytest==8.2.2',
+        'flake8==6.0.0'
     ],
     entry_points={
         'console_scripts': [
-            'run_optimization=production_optimization.main:main',
+            'run_optimization=production_optimization.main:run_optimization',
         ],
     },
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
 )
